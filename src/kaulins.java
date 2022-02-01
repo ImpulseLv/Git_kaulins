a24b3b86906fed7e89562537ea79c5215e6a3abc
Kanders <gints.kanders@inbox.lv>
2022-02-01 11:45:09 +0200

    Manas veiktas izmainas ImpulseLv programa

--- src/kaulins.java
+++ src/kaulins.java
@@ -12,11 +12,29 @@ public class kaulins {
 	}
 	
 	public static void main(String[] args) {
-		int reizes;
 		Scanner scan = new Scanner(System.in);
+		int reizes,izvele;
+		do{
+			System.out.println("1- mest kaulinu | 2 - apturet programu");
+		izvele = scan.nextInt();
+		switch(izvele){	
+		
+		case 1:	
+		do{
 		System.out.println("Cik reizes mest kauliņu?");
 		reizes = scan.nextInt();
+		}while(reizes<1);
 		mestKaulinu(reizes);
-		scan.close();
+		break;
+		
+		case 2:
+		System.out.println("programa aptureta!");
+		break;
+
+		}
+	}while(izvele!=2);
+	scan.close();
+	
 	}
 }
+
